bd7ab022237e99fcf11a3d15e805ef7f245b2c46
Harsh Joshi <hjoshi@sierrasystemsdev.com>
2018-12-12 08:55:09 -0700

    q4

--- BMEProjects/BMEProject-Core/BMEProject-Core/q4.cs
+++ BMEProjects/BMEProject-Core/BMEProject-Core/q4.cs
@@ -10,7 +10,34 @@ namespace Bme121
         static void Main()
         {
             const string path = "TheEnd.txt";
-
+            int counter = 1;
+            var lines = readData(path);
+            for (int i = 0; i < lines.Length-1; i+=2)
+            {
+                var endTime = "";
+                if (i + 3 > lines.Length)
+                {
+                    endTime = lines[i].Trim() + ",999";
+                }
+                else
+                {
+                    endTime = lines[i + 2].Trim() + ",000";
+                }
+                var time = lines[i]+",000 -->" + endTime;
+                var caption = lines[i + 1];
+                Console.WriteLine(counter);
+                Console.WriteLine(time);
+                Console.WriteLine(caption);
+                Console.WriteLine();
+                counter++;
+            }
         }
+
+        static string[] readData(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            return lines;
+        }
+        
     }
 }
