978aafe9b2124dbdfe1e6279a2668f856015cced
AJ-Joshi <claw045@gmail.com>
2019-01-25 23:55:13 -0500

    Updated Lab2.cpp

--- BME1B/AJBMECPlus/Lab2.cpp
+++ BME1B/AJBMECPlus/Lab2.cpp
@@ -1,8 +1,8 @@
 #include "stdafx.h"
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+#include<string>
+#include<ctime>
+#include<fstream>
 //#include <__ASSERT.h>
 
 using namespace std;
@@ -13,9 +13,42 @@ class Polynomial { //declare Polynomial class
 public:
 	Polynomial(int A[], int size) {
 		data = new int[size];
+		while (A[size - 1] == 0) {
+			size--;
+		}
+		for (int i = size-1; i >= 0; i--) {
+			if (A[i] == 0) {
+				size--;
+			}
+			data[i] = A[i];
+		}
+	}
+	Polynomial() {
+		srand(time(0));
+		data = new int[rand() % 1001];
+		for (int i = 0; i < sizeof(data)-1; i++) {
+			data[i] = rand() % 1001;
+		}
+	}
+	Polynomial(string fileName) {
+		ifstream instream("polynomial.txt");
+		int in;
+		while (!instream.eof()) {
+			instream >> in;
+			data_size = in;
+		}
+		data = new int[data_size];
+		for (int i = 0; i < data_size - 1; i++) {
+			
+		}
+
+		instream.close();
 	}
 	~Polynomial() {
 		delete [] data;
+	}
+	bool operator==(const Polynomial& target) {
+
 	}
 };
 
