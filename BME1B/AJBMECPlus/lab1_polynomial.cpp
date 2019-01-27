fe3af864b444670bba8a35c81adbb91396eeae89
Harsh Joshi (Sierra Contractor) <HJoshi@sierrasystemsdev.com>
2019-01-27 15:01:44 -0700

    operator overloading

--- BME1B/AJBMECPlus/lab1_polynomial.cpp
+++ BME1B/AJBMECPlus/lab1_polynomial.cpp
@@ -7,10 +7,10 @@
 
 using namespace std;
 
-class Polynomial { //declare Polynomial class
+class Polynomial { 
 	int* data;
 	int data_size;
-public:
+public:	
 	Polynomial(int A[], int size) {
 		data_size = size;
 		data = new int[data_size];
@@ -19,7 +19,7 @@ public:
 		}
 	}
 	Polynomial() {
-		srand(time(NULL));
+		srand(time(0));
 		data_size = rand() % 1000 + 1;
 		data = new int[data_size];
 		
@@ -47,41 +47,153 @@ public:
 		}
 		instream.close();
 	}
-	~Polynomial() {
-		if (data) {
+	~Polynomial() {		
 			delete[] data;
-			data = NULL;
-		}
 	}
 	bool operator==(const Polynomial& target) {
-		*this == target;
+		bool isValid = true;
+		if (this->data_size != target.data_size)
+			return false;
+		for (int i = 0; i < target.data_size; i++) {
+			if (this->data[i] != target.data[i]) {
+				isValid = false;
+				break;
+			}
+		}
+		return isValid;
+	}
+	Polynomial operator+(const Polynomial& target)
+	{
+		Polynomial result;
+		for (int i = 0; i < target.data_size; i++) {
+			result.data[i] = this->data[i] + target.data[i];
+		}
+		result.data_size = target.data_size;
+		return result;
+	}
+	Polynomial operator-(const Polynomial& target)
+	{
+		Polynomial result;
+		for (int i = 0; i < target.data_size; i++) {
+			result.data[i] = this->data[i] - target.data[i];
+		}
+		result.data_size = target.data_size;
+		return result;
+	}
+	Polynomial operator*(const Polynomial& target)
+	{
+		Polynomial result;
+		result.data_size = target.data_size + data_size - 1;
+		for (int i = 0; i < result.data_size; i++)
+		{
+			result.data[i] = 0;
+		}
+		for (int i = 0; i < this->data_size; i++) {
+			for (int j = 0; j < target.data_size; j++) {
+				int resultValue = this->data[i] * target.data[j];
+				int sumValue = result.data[i + j];
+				result.data[i + j] = resultValue+sumValue;
+			}
+		}		
+		return result;			
+	}
+	Polynomial derivative()
+	{
+		if (data_size == 0)
+		{
+			Polynomial emptyPoly(0,0);
+			return emptyPoly;
+		}
+		Polynomial result;
+		result.data_size = data_size - 1;
+		for (int i = 0; i < data_size; i++) {
+			result.data[i] = ((i+1) * data[i+1]);
+		}		
+		return result;
 	}
 	void print() {
-		// 1st term
-		std::cout << data[data_size - 1] << "x^" << data_size - 1;
-		// remaining terms
-		for (int i = data_size - 2; i >= 0; i--)
-			std::cout << " + " << data[i] << "x^" << i;
-		return;
+		
+		for (int i = 0; i < data_size; i++)
+		{
+			cout << data[i];
+			if (i != 0)
+				cout << "x^" << i;
+			if (i != data_size - 1)
+				cout << " + ";
+		}
 	}
+	friend class PolynomialTest;
 };
 
+class PolynomialTest {
+	
+public:
+	bool test_constructor_array() {
+		bool isValid = true;
+		int testArray[5] = { 19,59,4,41,8 };
+		Polynomial polyWithArray(testArray, 5);
+		//individually test coefficients
+		for (int i = 0; i < 5; i++)
+		{
+			if (polyWithArray.data[i] != testArray[i])
+			{
+				isValid = false;
+				break;
+			}
+		}
+		return isValid;
+	}
+	bool test_constructor_string() {
+		bool isValid = true;
+		Polynomial polyWithString("Polynomial.txt");
+		int testArray[8] = { 1,16,-5,6,1,9,8,22 };
+		for (int i = 0; i < 5; i++)
+		{
+			if (polyWithString.data[i] != testArray[i])
+			{
+				isValid = false;
+				break;
+			}
+		}
+		return isValid;
+
+	}
+	
+	bool test_equals_operator() {
+		
+		Polynomial polyWithString("Polynomial.txt");
+		int testArray[8] = { 1,16,-5,6,1,9,8,22 };
+		Polynomial polyWithArray(testArray, 8);
+		return polyWithString == polyWithArray;
+	}
+	
+	void run() {
+		//test 1 - constructor with array
+		if (test_constructor_array())
+			cout << "test_constructor_array Passed" << endl;
+		else
+			cout << "test_constructor_array Failed" << endl;
+		//test 2 - constructor with string
+		if (test_constructor_string())
+			cout << "test_constructor_string Passed" << endl;
+		else
+			cout << "test_constructor_string Failed" << endl;
+		//test 3 - test equal operator
+		if (test_equals_operator())
+			cout << "test_equals_operator Passed" << endl;
+		else
+			cout << "test_equals_operator Failed" << endl;
+		
+
+	}
+
+};
 int main(){
-	//test default constructor
-	Polynomial poly = Polynomial();
-	cout << "Polynomial Default Constructor=";
-	poly.print();
+	
 
 	//test constructor with array
-	int testArray[5] = { 19,59,4,41,8 };
-	Polynomial polyWithArray(testArray, 5);
-	cout << "\n\nPolynomial Array Constructor =";
-	polyWithArray.print();
-
-	//test constructor with string parameter
-	Polynomial polyWithString("Polynomial.txt");
-	cout << "\n\nPolynomial String Constructor =";
-	polyWithString.print();
+	PolynomialTest testClass = PolynomialTest();
+	testClass.run();	
 	cout << "\n\n";
 	return 0;
 }
